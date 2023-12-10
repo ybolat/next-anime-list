ecbb1acc911dee77d2a56dbe1bc44ebf41dc9848
ybolat <erabeast@mail.ru>
2023-12-10 20:56:05 +0600

    first commit

---
+++ components/LoadMore.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+import {useEffect, useState} from "react";
+import {fetchAnime} from "@/app/action";
+import AnimeCard, {AnimeProp} from "@/components/AnimeCard";
+
+let page = 2;
+
+function LoadMore() {
+    const {ref, inView} = useInView();
+    const [data, setData] = useState<AnimeProp[]>([]);
+
+    useEffect(() => {
+        if (inView) {
+            fetchAnime(page).then((res) => {
+                setData(prevState => [...prevState, ...res]);
+                page++;
+            });
+        }
+    }, [inView]);
+
+    return (
+        <>
+            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+                {data.map((item: AnimeProp, index) => {
+                    const idx = index % 8;
+                    return <AnimeCard key={item.id} anime={item} index={idx}/>
+                })}
+            </section>
+            <section className="flex justify-center items-center w-full">
+                <div ref={ref}>
+                    <Image
+                        src="./spinner.svg"
+                        alt="spinner"
+                        width={56}
+                        height={56}
+                        className="object-contain"
+                    />
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default LoadMore;
